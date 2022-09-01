71eb9f7b2e82fdc4d7d479426446bc6b29a2c844
KYOJIN <gjhwang5510@gmail.com>
2022-09-02 07:04:24 +0900

    ADD: DAY05.js

---
+++ DAY05.js
@@ -0,0 +1,16 @@
+// 순위: 53921
+// 획득한 점수: 1088
+// 해결한 문제 수: 21
+// 서울에서 김서방 찾기
+
+function solution(seoul) {
+  const location = seoul.indexOf("Kim");
+  console.log(`김서방은 ${location}에 있다`);
+}
+
+function solution(seoul) {
+  console.log(`김서방은 ${seoul.indexOf("Kim")}에 있다`);
+}
+
+seoul = ["Jane", "Kim"];
+solution(seoul);
