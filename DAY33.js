34e1058e4865d22f30fbc9efb7a57a25207b358c
KYOJIN <gjhwang5510@gmail.com>
2022-09-30 15:02:22 +0900

    ADD: DAY33.js

---
+++ DAY33.js
@@ -0,0 +1,56 @@
+// 숫자 문자열과 영단어
+// 문제 설명
+// img1.png
+
+// 네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.
+
+// 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
+
+// 1478 → "one4seveneight"
+// 234567 → "23four5six7"
+// 10203 → "1zerotwozero3"
+// 이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나,
+// 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다. s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+// if 문은 ( ) 조건식 평가를 한후 한번만 실행하고 끝남
+// 그런데 while 문은 ( ) 조건식 평가 후에 실행하고 다시 돌아와서 평가하는 방식 if문쓰면 replaceAll / while문 쓰면 replace도 가능.
+function solution(s) {
+    let num = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    for (let i = 0; i <= num.length; i++) {
+        if (s.includes(num[i])) {
+            s = s.replaceAll(num[i], i);
+        }
+    }
+    return Number(s);
+}
+
+function solution(s) {
+    let num = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    for (let i = 0; i <= num.length; i++) {
+        while (s.includes(num[i])) {
+            s = s.replace(num[i], i);
+        }
+    }
+    return Number(s);
+}
