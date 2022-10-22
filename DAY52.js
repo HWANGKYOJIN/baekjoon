8f99977e00fd21b711a7438c6ded46976918a529
KYOJIN <gjhwang5510@gmail.com>
2022-10-22 23:21:16 +0900

    ADD:DAY52.js

---
+++ DAY52.js
@@ -0,0 +1,23 @@
+// 프로그래머스에 레벨0 문제가 생겨서 차근차근 풀어보기로함.
+
+function solution(num1, num2) {
+    return num1 % num2;
+}
+
+function solution(num1, num2) {
+    return num1 * num2;
+}
+
+function solution(num1, num2) {
+    return num1 + num2;
+}
+function solution(num1, num2) {
+    return num1 - num2;
+}
+function solution(num1, num2) {
+    return num1 === num2 ? 1 : -1;
+}
+function solution(age) {
+    let answer = 2022;
+    return answer - age + 1;
+}
